99d5f5714356fdc6f9ae82322e6c37b95747bcec
SakshiBawankar28 <sakshibawabkar@gmail.com>
2024-05-26 19:05:32 +0530

    Number Pattern Code

---
+++ PatternProgram/src/AlphabetPattern/Program07.java
@@ -0,0 +1,21 @@
+package AlphabetPattern;
+
+public class Program07 {
+
+	public static void main(String[] args)
+	{
+		
+		//for rows
+		for (char i='A'; i<='E'; i++) 
+		{
+			//for column
+			for (char j='A'; j<='E'; j++) 
+			{
+				//for printing
+				System.out.print(j+ " ");
+			}
+			System.out.println();
+		}
+	}
+
+}
---
+++ PatternProgram/src/AlphabetPattern/Program09.java
@@ -0,0 +1,17 @@
+package AlphabetPattern;
+
+public class Program09 {
+
+	public static void main(String[] args) 
+	{
+		for (char i ='E'; i>='A'; i--) 
+		{
+			for (char j='A'; j<=i; j++) 
+			{
+				System.out.print(j + " ");
+			}
+			System.out.println();
+		}
+	}
+
+}
